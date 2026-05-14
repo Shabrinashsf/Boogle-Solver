cea4c9029ce1726d3445ef0ad5352053e20dbce6
Shabrina Amalia <shabrinaamalia860@gmail.com>
2026-05-14 22:55:52 +0700

    fix: fix output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingIncludes: {
+    "/api/solve": ["./public/dictionary.txt"],
+  },
 };
 
 module.exports = nextConfig;
